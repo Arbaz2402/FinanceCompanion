8d9cbb22232e4527acdde6b910b373970c25ccba
Arbaz Kaladiya <kaladiya.arbaz@gmail.com>
2026-04-02 22:12:13 +0530

    Initial Commit

---
+++ FinanceCompanion/FinanceCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceCompanionApp.swift
+//  FinanceCompanion
+//
+//  Created by Arbaz Kaladiya on 02/04/26.
+//
+
+import SwiftUI
+
+@main
+struct FinanceCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
